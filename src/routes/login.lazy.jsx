a132194fc8e4a4a307eb2a79741af1e3e32c80dd
fariqmanaf <fariqmanaf24@gmail.com>
2024-10-25 20:56:22 +0700

    hi risky

--- src/routes/login.lazy.jsx
+++ src/routes/login.lazy.jsx
@@ -4,19 +4,53 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import { useState } from "react";
 
 export const Route = createLazyFileRoute("/login")({
   component: Login,
 });
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    /* hit the login API */
+    // define the request body
+    const body = {
+      email,
+      password,
+    };
+
+    const response = await fetch("http://localhost:4000/auth/login", {
+      body: JSON.stringify(body),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+
+    if (!result.success) {
+      console.log(result.data);
+      return;
+    }
+
+    alert(result.message);
+  };
+
   return (
     <Row className="mt-5">
+      <h1>{email}</h1>
+      <h1>{password}</h1>
       <Col className="offset-md-3">
         <Card className="text-center">
           <Card.Header>Login</Card.Header>
           <Card.Body>
-            <Form>
+            <Form onSubmit={onSubmit}>
               <Form.Group
                 as={Row}
                 className="mb-3"
@@ -26,7 +60,13 @@ function Login() {
                   Email
                 </Form.Label>
                 <Col sm="10">
-                  <Form.Control type="email" placeholder="Email" required />
+                  <Form.Control
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </Col>
               </Form.Group>
               <Form.Group
@@ -42,13 +82,17 @@ function Login() {
                     type="password"
                     placeholder="Password"
                     required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </Col>
+                <div className="d-grid gap-2">
+                  <Button type="submit" variant="primary">
+                    Login
+                  </Button>
+                </div>
               </Form.Group>
             </Form>
-            <div className="d-grid gap-2">
-              <Button variant="primary">Login</Button>
-            </div>
           </Card.Body>
         </Card>
       </Col>
